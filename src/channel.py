1d57e19f523199ae3580c94cdb2b62b580db6c92
Andrey Mezentsev <mezentsew@mail.ru>
2023-12-01 16:55:07 +0300

    Create eight magic methods and their are add to module channel.py.

--- src/channel.py
+++ src/channel.py
@@ -7,7 +7,9 @@ from googleapiclient.discovery import build
 
 
 class Channel:
-    """Класс для ютуб-канала"""
+    """
+    Класс для ютуб-канала.
+    """
 
     # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
     api_key: str = os.getenv("YOUTUBE_API_KEY")
@@ -16,9 +18,11 @@ class Channel:
     youtube = build("youtube", "v3", developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут
-        подтягиваться по API."""
-        self.channel__id = channel_id
+        """
+        Экземпляр инициализируется id канала. Дальше все данные будут
+        подтягиваться по API.
+        """
+        self.__channel_id = channel_id
         self.channel = (
             self.youtube.channels()
             .list(id=channel_id, part="snippet,statistics")
@@ -27,7 +31,7 @@ class Channel:
 
         self.title = self.channel["items"][0]["snippet"]["title"]
         self.video_count = self.channel["items"][0]["statistics"]["videoCount"]
-        self.url = f"https://www.youtube.com/channel/channel_id"
+        self.url = f"https://www.youtube.com/channel/{self.__channel_id}"
         self.id = self.channel["items"][0]["id"]
         self.description = self.channel["items"][0]["snippet"]["description"]
         self.subscriber_count = self.channel["items"][0]["statistics"][
@@ -38,21 +42,84 @@ class Channel:
     @property
     def channel_id(self) -> str:
         """
-        Вовращает id канала
+        Возвращает id канала.
         """
         return self.__channel_id
+
+    def __str__(self):
+        """
+        Возвращает название канала и ссылку в удобном формате.
+        :return: описание канала с ссылкой.
+        """
+        return f"{self.title} ({self.url})"
+
+    def __add__(self, other):
+        """
+        Складывает подписчиков двух каналов.
+        :return: результат сложения подписчиков.
+        """
+        return int(self.subscriber_count) + int(other.subscriber_count)
+
+    def __sub__(self, other):
+        """
+        Вычитает разность подписчиков двух каналов.
+        :return: результат вычитания подписчиков.
+        """
+        return int(self.subscriber_count) - int(other.subscriber_count)
+
+    def __lt__(self, other):
+        """
+        Сравнивает подписчиков двух каналов.
+        :return: True, если у первого канала меньше
+        подписчиков, в противном случае False.
+        """
+        return self.subscriber_count < other.subscriber_count
+
+    def __le__(self, other):
+        """
+        Сравнивает подписчиков двух каналов.
+        :return: True, если у первого канала меньше или
+        такое же кол-во подписчиков, в противном случае False.
+        """
+        return self.subscriber_count <= other.subscriber_count
+
+    def __gt__(self, other):
+        """
+        Сравнивает подписчиков двух каналов.
+        :return: True, если у первого канала больше подписчиков,
+        в противном случае False.
+        """
+        return self.subscriber_count > other.subscriber_count
+
+    def __ge__(self, other):
+        """
+        Сравнивает подписчиков двух каналов.
+        :return: True, если у первого канала больше или
+        такое же кол-во подписчиков, в противном случае False.
+        """
+        return self.subscriber_count >= other.subscriber_count
+
+    def __eq__(self, other):
+        """
+        Сравнивает подписчиков двух каналов.
+        :return: True, если у обоих каналов одинаковое кол-во подписчиков,
+        иначе False.
+        """
+        return self.subscriber_count == other.subscriber_count
 
     @classmethod
     def get_service(cls) -> Any:
         """
-        Класс-метод, возвращающий объект для работы с YouTube API
+        Класс-метод, возвращающий объект для работы с YouTube API.
         """
         channel_id = os.getenv("YOUTUBE_API_KEY")
         cls.youtube = build("youtube", "v3", developerKey=channel_id)
         return cls.youtube
 
     def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
+        """
+        Выводит в консоль информацию о канале.
+        """
         pprint(self.channel)
 
     def to_json(self, filename: Any) -> Any:
@@ -70,7 +137,3 @@ class Channel:
         }
         with open(filename, "w", encoding="utf-8") as file:
             json.dump(channel_info, file, indent=4, ensure_ascii=False)
-
-    @channel_id.setter
-    def channel_id(self, value):
-        self.__channel_id = value
